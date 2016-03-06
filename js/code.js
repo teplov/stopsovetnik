e7213701ce81622e296e4f590e3d3fb722d7fea6
Roman Teplov <teplov@gmail.com>
2016-03-06 13:43:09 +0300

    Add premium plan

---
+++ js/code.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+  var hash = window.location.hash || null;
+
+console.log(hash);
+  if (hash && hash === '#lightbox') {
+      $('.overlay, .lightbox').show();
+  }
+
+
+  $('.overlay').click(function() {
+    $('.overlay, .lightbox').hide();
+    window.location.hash = '';
+  });
+
+  $('.open_lightbox').click(function () {
+    $('.overlay, .lightbox').show();
+  });
+
+});
